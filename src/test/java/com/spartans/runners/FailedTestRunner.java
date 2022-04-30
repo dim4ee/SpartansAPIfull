77d888aa3bef6f93718f0b0b3ec44e102892cb6a
Dimo Velikov <dvelikov24@gmail.com>
2022-04-30 13:16:39 -0400

    initial commit

---
+++ src/test/java/com/spartans/runners/FailedTestRunner.java
@@ -0,0 +1,13 @@
+package com.spartans.runners;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "com/spartans/step_definitions",
+        features = "@target/rerun.txt"
+
+)
+public class FailedTestRunner {
+}
